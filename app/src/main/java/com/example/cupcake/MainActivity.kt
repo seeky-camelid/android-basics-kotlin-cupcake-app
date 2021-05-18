4d3228d68b005ae8bf504d0b4620dab6a8be08cb
SiCong Li <sicong.li@arm.com>
2021-05-18 08:16:50 +0100

    Use Navigation Component's action bar

--- app/src/main/java/com/example/cupcake/MainActivity.kt
+++ app/src/main/java/com/example/cupcake/MainActivity.kt
@@ -15,7 +15,10 @@
  */
 package com.example.cupcake
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 
 /**
  * Activity for cupcake order flow.
@@ -23,3 +26,15 @@ import androidx.appcompat.app.AppCompatActivity
  * takes in a layout that will be inflated as part of super.onCreate(savedInstanceState).
  */
 class MainActivity : AppCompatActivity(R.layout.activity_main)
+{
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Get a hold of the navController associated with this nav host
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        // Show a title in the app bar based off of the destination's label, and display the Up button
+        // whenever you're not on a top-level destination.
+        setupActionBarWithNavController(navController)
+    }
+}
